d75e5ae6837f65df91bd3a042daf318ebd0a8c8e
Nolyn Janssen <nolyn.janssen@gmail.com>
2025-02-11 09:47:01 -0600

    Clean Up
    Cleaned up some of the empty lines, cleaned up/standardized my comments

--- cod1week3adventure1.0/cod1week3adventure1.0.cpp
+++ cod1week3adventure1.0/cod1week3adventure1.0.cpp
@@ -3,24 +3,22 @@ using namespace std;
 #include <string>
 #include <cstdlib>
 #include <ctime>
-// if the is a "?" before a comment, it was the instructions for the assignment
-
-
+																// if the is a "?" before a comment, it was the instructions for the assignment
 int main() {
 
 // ?welcome the player
 	srand(time(NULL));											// seeding, I admittedly don't fully understand this part, but whatevs
 	char pAnswer = 'y';											// setup for player input on intro questions
 	string playerName = "null";									// playerName, initialized with placeholder text, wasn't in the instructions, but I liked the idea of having a custom name
-	int pHealth = 10;
-	char cont = 'y';			//I wanted this named "continue" but thats a thing so cont it is
-	int turn = 0;
-	int pBlock = (rand() % 4) + 1;
-	int pAttack = (rand() % 4) + 1;
-	int pBlockDefault = pBlock;
-	int battlesWon = 0;
+	int pHealth = 10;											// player health
+	char cont = 'y';											// variable for if the player wants to continue, I wanted this named "continue" but thats a thing so cont it is
+	int turn = 0;												// turn counter
+	int pBlock = (rand() % 4) + 1;								// player block value/stat, is randomized 1-4
+	int pAttack = (rand() % 4) + 1;								// player attack value/stat, is randomized 1-4
+	int pBlockDefault = pBlock;									// variable to be able to reset block in case of stat modifiers
+	int battlesWon = 0;											// counter for the number of battles won (duh)
 	string pInput = "nullbutinput";
-	cout << "\tHello!\n -Please enter your name-\n\t";				// request playerName from player
+	cout << "\tHello!\n -Please enter your name-\n\t";			// request playerName from player
 	getline(cin, playerName);									// assign playerName
 	cout << "\n\n I see..." << playerName << ", what a nice name!\n Would you like to go an adventure " << playerName << "?\n";
 	cin >> pAnswer;
@@ -38,22 +36,21 @@ int main() {
 			cout << "\nI wish you luck " << playerName << ", and I shall speak with shortly.";
 
 			cout << "\n\n\n In battle, type 'attack' to make a slash at your enemy, or 'block' for a 50% bonus to your defences.";
-
-			
-			while (cont == 'y'){												// Man i'm only like 60% sure of what I'm doing
+	
+			while (cont == 'y'){												// man i'm only like 60% sure of what I'm doing
 				turn = 0;
 				cout << "\nA new creature stands before you.";						
-				int creatureAttack = (rand() % 3) + 1;							// Determine creatures attack power
-				int creatureAction = 1;											// This will be the creatures move each turn, it will be set randomly, a 1 is a block, a 2 is an attack
-				while (pHealth > 0 && turn < 4)									//Loop checking the two fail conditions each turn
+				int creatureAttack = (rand() % 3) + 1;							// determine creatures attack power, randomized 1-3
+				int creatureAction = 1;											// this will be the creatures move each turn, it will be set randomly, a 1 is a block, a 2 is an attack
+				while (pHealth > 0 && turn < 4)									// loop checking the two fail conditions each turn
 				{
-					
-					pBlock = pBlockDefault;										// Probably not optimal, but resets the block to the proper value in case the player decides to block
-					creatureAction = (rand() % 2) + 1;							// Determines creatures action this turn
+		
+					pBlock = pBlockDefault;										// probably not optimal, but resets the block to the proper value in case the player decides to block
+					creatureAction = (rand() % 2) + 1;							// determines creatures action this turn
 					cout << "\nWhat shall you do?\n";							//!!!This keeps saying this twice, and I don't know why!!!
-					getline(cin, pInput);										// Player inputs actions
-					if (pInput == "attack") {									//Condition for if player attacks
-						if (creatureAction == 2)								//checks for creatures action, if it blocked, oh no, if it didn't, player kills it
+					getline(cin, pInput);										// player inputs actions
+					if (pInput == "attack") {									// condition for if player attacks
+						if (creatureAction == 2)								// checks for creatures action, if it blocked, oh no, if it didn't, player kills it
 						{
 							cout << "The creature was slain!";
 							battlesWon++;
@@ -61,16 +58,15 @@ int main() {
 						}
 						if (creatureAction == 1) {
 							cout << "The creature blocked.";
-						}
-						
+						}		
 					}
-					if (pInput == "block")										//If player blocked, it checks to see if the creature was attacking and if so, did it hit regardless
+					if (pInput == "block")										// if player blocked, it checks to see if the creature was attacking and if so, did it hit regardless
 					{
 						pBlock = pBlock * 1.5;
 						if (creatureAction == 2) {
 							cout << "The creature attacked, and it...";
 							if (creatureAttack >= pBlock) {
-								cout << "hit! Ouch! You lost" << creatureAttack << " health!";
+								cout << "hit! Ouch! You lost " << creatureAttack << " health!";
 								pHealth = pHealth - creatureAttack;
 							}
 							else {
@@ -80,55 +76,30 @@ int main() {
 						if (creatureAction == 1) {
 							cout << "The creature cowers in fear as you stand in prepartion of a blow...way to waste a turn.";
 						}
-					}
-					
-					
-					turn++;						//Adds turn
-				}//end of encounter loop
-				if (pHealth <= 0) {							//Check if loop exited due to death
+					}				
+					turn++;													// adds turn
+				}// end of encounter loop
+				if (pHealth <= 0) {											// check if loop exited due to death
 					cout << "\nOwie, you died.";
 					break;
 				}
-				else if (turn >= 4) {						//Check if loop exited due to time
+				else if (turn >= 4) {										// check if loop exited due to time
 					cout << "\nThe battle waned for too long, reinforcements arrived of the enemy arrived, and you were overwhelmed. You died.";
 					break;
 				}
-				cout << "\nContinue?";						//if loop exited due to victory, asks if the player wants to continue
-				cin >> cont;								//Takes player input to see if gameplay loop should continue
-			}//end of play loop
-
+				cout << "\nContinue?";										// if loop exited due to victory, asks if the player wants to continue
+				cin >> cont;												// takes player input to see if gameplay loop should continue
+			}// end of play loop
 		}
 		if (pAnswer == 'n') {							
 			cout << "\nI see...there is no shame in knowing ones one limits, depart from this place then!";
-			cout << "\n\n\n\n\.";										// I don't like the command window game ending with the exit instructions of vstudio, so this adds space			
-
+			cout << "\n\n\n\n\.";											// I don't like the command window game ending with the exit instructions of vstudio, so this adds space			
 			return 0;
 		}
 	}
 	if (pAnswer == 'n') {
 		cout << "Bah! Coward, leave this place at once!\n *The wizard points his staff at you, and your vision goes black*";
 	}
-	cout << "\nThe game is over. You won " << battlesWon << " battles, good job!";							//Text to tell player their score
-
-
-// ?set up the game
-// ?int health = 10, attack, block, turns = 0
-// ?seed the random number generator
-// ?start the loop
-// ?add 1 to turns
-// ?start the encounter
-// ?randomly generate numbers for attack(range = 0 - 4) and block(range = 0 - 4)
-// ?if block is greater or equal to attack, successful block
-// ?otherwise, subtract attack value from health.
-// ?ask the player if they would like to keep adventuring. if they don't, break out of the loop.
-// ?keep looping while health is greater than zero and fewer than 4 turns have happened
-
-// ?if health is greater than 0, congratulate player
-// ?otherwise, tell the player they're dead.
-
-cout << "\n\n\n\n\.";										// I don't like the command window game ending with the exit instructions of vstudio, so this adds space			
+	cout << "\nThe game is over. You won " << battlesWon << " battles, good job!";				// text to tell player their score
+cout << "\n\n\n\n\.";																			// I don't like the command window game ending with the exit instructions of vstudio, so this adds space			
 }// end of main
-
-
-
-
