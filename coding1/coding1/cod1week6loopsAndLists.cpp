52e530a5ad04569692a5718b41accaa6a6bde998
Nolyn Janssen <nolyn.janssen@gmail.com>
2025-02-25 10:09:03 -0600

    Test
    Please Work

---
+++ coding1/coding1/cod1week6loopsAndLists.cpp
@@ -0,0 +1,18 @@
+// Nolyn Janssen, Coding 1, Week 6 of Spring 25, Feb 25th 2025
+// Loops and Lists
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Making an array
+	// Array is an ordered list of a single type of variable
+		
+	// Creat an Array of integers with a [size] of 10
+	
+	const int MAX_SIZE = 10;			// Constant variable for max size, cannot be changed, ALL_CAPS format for constants
+
+	int element = 0;					// Tracks where array location is
+
+	int scores[MAX_SIZE];		
+}
