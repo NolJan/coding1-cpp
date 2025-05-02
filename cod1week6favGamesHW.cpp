bed899df3b825dd8e4c2e6ec07cbb2a27a2f6b13
Nolyn Janssen <nolyn.janssen@gmail.com>
2025-05-02 13:37:07 -0500

    Coding 1 Week 6 favGames finish
    Finished this assignment, it works

--- cod1week6favGamesHW.cpp
+++ cod1week6favGamesHW.cpp
@@ -8,59 +8,66 @@ int main() {
 	const int MAX_SIZE = 20;								// Max size of the array, I could've just done 20, but good form or something, idk
 	string favGames[MAX_SIZE];								// The string thats the favGames list, size of 20 (MAX_SIZE)
 	string input;											// You wouldn't believe what this does
-	cout << "Howdy, welcome to the program that will allow you make and manipulate a list of your favorite games!\n";
+	cout << "Howdy, welcome to the program that will allow you make and manipulate a list of your favorite games!\n";		// Intro to explain purpose
 	do {
-		cout << " Please Input One of the Following\nAdd\tRemove\tEdit\tShow\tQuit\n";
-		cin >> input;
-			break;
+		cout << " Please Input One of the Following\nAdd\tRemove\tEdit\tShow\tQuit\n";										// List Command Options
+		cin >> input;																										// Take initial input
+		if (input == "Add" || input == "add") {																								// Check if Add was prompted
+			cout << "Please name the game you would like to add!\n";														// Get game from user
+			cin >> favGames[gameCount];																						// Take user input, assign it to the most recent index
+			gameCount++;																									// Update the counter of games, functions as a fix for the human vs computer number formatting, 
+			cout << "The game in slot " << gameCount <<  " is now " << favGames[gameCount-1] << ".\n";						// Inform the user of the game added and where in the list
+ 		}
+		if (input == "Edit" || input == "edit") {																			// Check for Edit command input
+			cout << "What game would you like to edit?\n";																	// Prompt for game to edit
+			cin >> input;																									
+			for (int i = 0; i <= gameCount; i++) {																				
+				if (input == favGames[i]) {																					// Loop as many times as the inputted games count is, checking if the promptet edit is in the list currently
+					cout << "What would you like to edit " << favGames[i] << " to?\n";										// If match found, prompt for the replacement
+					cin >> favGames[i];
+					cout << "The game in slot " << i + 1 << " is now " << favGames[i] << "\n";
+					break;																									// Inform user of edit success, break to prevent false negative cout
+				}	
+				if (i == gameCount) {																						// Might be a better way to do this, but this allows me to only do this at the end
+					cout << "That game is not found in the list, would you like to view the current list?\n";				// If there isn't a match, prompts user to see if they would 
+					cin >> input;																							// Asks for user if they want to see the list
+					if (input == "Yes" || input == "yes" || input == "y" || input == "Y") {									// Just different yes input formats
+						for (int i = 0; i <= gameCount; i++) {
+							cout << i + 1 << ". " << favGames[i] << "\n";													// Loop that listall entries
+						}
+					}
+					break;
+				}
+			}
+		}
+		if (input == "Remove" || input == "remove") {																		// Check for if the input is remove
+			cout << "What game would you like to remove?\n";																// Ask for input, assign it to the input holder
+			cin >> input;
+			for (int i = 0; i < gameCount; i++) {																			// Same loop and break as the Edit branch, just delete it instead
+				if (input == favGames[i]) {																						
+					favGames[i] = "";
+					cout << input << " has been removed from slot" << i + 1 << "\n";
+					break;
+				}
+				if (i == gameCount) {
+					cout << "That game is not found in the list, would you like to view the current list?\n";
+					cin >> input;
+					if (input == "Yes" || input == "yes" || input == "y" || input == "Y") {
+						for (int i = 0; i < gameCount; i++) {
+							cout << i + 1 << ". " << favGames[i] << "\n";
+						}
+					}
+					break;
+				}
+			}
+		}
+		if (input == "Show" || input == "show") {																			// Check if input is show
+			for (int i = 0; i < gameCount; i++) {																			// Loop to output list formatted numerically
+				cout << i + 1 << ". " << favGames[i] << "\n";
+			}
+		}
+		if (input == "Quit" || input == "quit") {																			// Check for input being "Quit"
+			return 0;																										
+		}
 	} while (true);
 }
-
-
-/*
-	Be Pseudo Code Fr, for FavGames Assignment
-
-	Setup Stuff
-		int gameCount = 0;
-	start loop
-	0. Greeting
-		some text
-		explain input options
-		prompt for input
-	1. Add
-		ask the player "what game they want to add"
-		get input from player
-		add input(game) to array and increment
-			gameCount
-		(optional) tell player what name they've added
-	2. Edit
-		ask player "what game do you want to edit"
-		get input from player
-		loop through array to index [gameCount]						//honestly should just make a "search" function (upon further thought, idk if i can do that with my current knowledge set)
-			check each element for if it matches the edit query
-				loop, if input == array[index]
-			if match found
-				ask player "what would you like to edit it to"
-				get input
-				replace element at index gameCount with input
-			if none found
-				tell player that no match is found
-				(optional) prompt player for if they would like to see the list
-					get input
-					show list (see 4)
-	3. Remove
-		ask player "what game would you like to remove"
-		get input from player
-		loop through array to index [gameCount]						//again, make a search function for this reason, it repeats twice
-		if match found
-			(optional) ask player if they are sure they want to delete
-				if yes, do it, if no, reset to input promot
-			delete it
-		if match not found
-			tell player that no match is found
-				optionally, again as in 2, ask player if they would like to see a list to check for what they want
-	4. Show
-		loop through array using for loop, just output the list
-	5. Quit
-		yeah, quit, break, whatever
-	*/
